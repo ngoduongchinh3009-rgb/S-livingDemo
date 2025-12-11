51f3a7d3d18d665f275151fe28d1d22ad58edee5
ngoduongchinh3009-rgb <ngoduongchinh3009@gmail.com>
2025-12-11 16:02:10 +0700

    learn-more

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const learnMoreBtns = document.querySelectorAll(".learn-more");
+const productDetail = document.querySelector(".product-detail");
+const backBtn = document.querySelector(".back-btn");
+const activeDescribeBtn = document.querySelector(".active-describe");
+const activeSpecificationsBtn = document.querySelector(
+  ".active-specifications"
+);
 
-learnMoreBtns.forEach((learnMoreBtns) => {
-  learnMoreBtns.addEventListener("click", function () {
+const describeBox = document.querySelector(".describe");
+const specificationsBox = document.querySelector(".specifications");
+
+learnMoreBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
     const item = this.closest(".item");
 
     const name = item.querySelector("h2").innerText;
@@ -26,20 +35,9 @@ learnMoreBtns.forEach((learnMoreBtns) => {
   });
 });
 
-const productDetail = document.querySelector(".product-detail");
-const backBtn = document.querySelector(".back-btn");
-
 backBtn.addEventListener("click", () => {
   productDetail.style.display = "none";
 });
-
-const activeDescribeBtn = document.querySelector(".active-describe");
-const activeSpecificationsBtn = document.querySelector(
-  ".active-specifications"
-);
-
-const describeBox = document.querySelector(".describe");
-const specificationsBox = document.querySelector(".specifications");
 
 activeDescribeBtn.addEventListener("click", () => {
   describeBox.style.display = "block";
