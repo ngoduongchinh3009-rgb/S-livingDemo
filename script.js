df9938ab453c92eb9518a3f10af871b702045d35
ngoduongchinh3009-rgb <ngoduongchinh3009@gmail.com>
2025-12-15 17:52:20 +0700

    cart

--- script.js
+++ script.js
@@ -38,3 +38,48 @@ activeSpecificationsBtn.addEventListener("click", function () {
 backBtn.addEventListener("click", function () {
   productDetail.style.display = "none";
 });
+
+const addCartBtns = document.querySelectorAll(".add-cart");
+const cartIcon = document.querySelector(".icon-cart");
+const cartBox = document.querySelector(".cart-box");
+const cartImg = document.getElementById("cart-img");
+const cartName = document.getElementById("cart-name");
+const cartEng = document.getElementById("cart-eng");
+const cartBackBtn = document.querySelector(".cart-box .back-btn");
+
+let cart = [];
+addCartBtns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const product = this.closest(".best-sellers-list, .products-list");
+    const name = product.querySelector(".best-sellers-names").innerText;
+    const eng = product.querySelector(".best-sellers-names-e").innerText;
+    const img = product.querySelector("img").src;
+    addToCart(name, eng, img);
+  });
+});
+function addToCart(name, eng, img) {
+  const existedItem = cart.find((item) => item.name === name);
+  if (existedItem) {
+    existedItem.quantity++;
+  } else {
+    cart.push({
+      name: name,
+      eng: eng,
+      img: img,
+      quantity: 1,
+    });
+  }
+  showCart(cart[cart.length - 1]);
+}
+function showCart(item) {
+  cartImg.src = item.img;
+  cartName.innerText = item.name;
+  cartEng.innerText = item.eng;
+  cartBox.style.display = "block";
+}
+cartIcon.addEventListener("click", function () {
+  cartBox.style.display = "block";
+});
+cartBackBtn.addEventListener("click", function () {
+  cartBox.style.display = "none";
+});
